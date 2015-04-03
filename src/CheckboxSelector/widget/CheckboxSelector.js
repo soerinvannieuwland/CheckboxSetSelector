d64f6c088011c7bd0c833c4520d6c3b2e03d8cbb
Pauline Oudeman <pauline.van.der.kraats@mendix.com>
2015-04-03 15:00:57 +0200

    Tweaked setting references

--- src/CheckboxSelector/widget/CheckboxSelector.js
+++ src/CheckboxSelector/widget/CheckboxSelector.js
@@ -19,7 +19,7 @@ require([
         // Extra variables
         _selectAllBox: null,
         _readonly: null,
-		_firstTh: null,
+        _firstTh: null,
 
 
         /**
@@ -55,23 +55,23 @@ require([
             this._contextObj = obj;
 
             if (this._contextObj === null) {
-                
-				// Sorry no data no show!
-				domStyle.set(this.domNode, "display", "none");
+
+                // Sorry no data no show!
+                domStyle.set(this.domNode, "display", "none");
                 console.debug('CheckboxSelector  - update - We did not get any context object!');
             } else {
-				domStyle.set(this.domNode, "display", "initial");
-				this._readonly = this._contextObj.isReadonlyAttr(this.reference.split('/')[0]);
-                
-				// Subscribe to object updates.
-				this._resetSubscriptions();
-                
-				// Load data
+                domStyle.set(this.domNode, "display", "initial");
+                this._readonly = this._contextObj.isReadonlyAttr(this.reference.split('/')[0]);
+
+                // Subscribe to object updates.
+                this._resetSubscriptions();
+
+                // Load data
                 this._loadData();
             }
 
             callback();
-            
+
         },
 
         /**
@@ -93,8 +93,7 @@ require([
             //TODO, what will happen if the widget is disabled (set visible).
         },
 
-        unintialize: function () {
-        },
+        unintialize: function () {},
 
 
         /**
@@ -106,17 +105,17 @@ require([
 
             // To be able to just alter one variable in the future we set an internal variable with the domNode that this widget uses.
             this._wgtNode = this.domNode;
-			this._firstTh = domQuery('.first-th', this._wgtNode)[0];
-			
-			if (this.addSelectAll) {
+            this._firstTh = domQuery('.first-th', this._wgtNode)[0];
+
+            if (this.addSelectAll) {
                 console.debug('addSelectAll');
                 this._selectAllBox = domConstruct.create('input', {
                     type: 'checkbox'
                 });
-				
+
                 console.debug(this._selectAllBox);
-                
-				domConstruct.place(this._selectAllBox, this._firstTh);
+
+                domConstruct.place(this._selectAllBox, this._firstTh);
             }
 
         },
@@ -158,73 +157,74 @@ require([
 
         },
 
-		_resetSubscriptions: function () {
-			var entHandle = null, 
-				attrHandle = null, 
-				validationHandle = null;
+        _resetSubscriptions: function () {
+            var entHandle = null,
+                attrHandle = null,
+                validationHandle = null;
 
-			// Release handles on previous object, if any.
-			if(this._handles){
-				this._handles.forEach(function (handle, i) {
-					mx.data.unsubscribe(handle);
-				});
-			}
+            // Release handles on previous object, if any.
+            if (this._handles) {
+                this._handles.forEach(function (handle, i) {
+                    mx.data.unsubscribe(handle);
+                });
+            }
 
-			if (this._contextObj) {
-				entHandle = this.subscribe({
-					entity: this.reference.split('/')[1],
-					callback: lang.hitch(this,function(guid) {
-						this._loadData();
-					})
-				});
+            if (this._contextObj) {
+                entHandle = this.subscribe({
+                    entity: this.reference.split('/')[1],
+                    callback: lang.hitch(this, function (guid) {
+                        this._loadData();
+                    })
+                });
 
-				attrHandle = this.subscribe({
-					guid: this._contextObj.getGuid(),
-					attr: this.reference.split('/')[0],
-					callback: lang.hitch(this,function(guid,attr,attrValue) {
-						this._loadData();
-					})
-				});
+                attrHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    attr: this.reference.split('/')[0],
+                    callback: lang.hitch(this, function (guid, attr, attrValue) {
+                        this._loadData();
+                    })
+                });
 
-				validationHandle = mx.data.subscribe({
-					guid     : this._contextObj.getGuid(),
-					val      : true,
-					callback : lang.hitch(this,this._handleValidation)
-				});
+                validationHandle = mx.data.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    val: true,
+                    callback: lang.hitch(this, this._handleValidation)
+                });
 
-				this._handles = [entHandle, attrHandle, validationHandle];
-			}
-		},
-		
-		_handleValidation: function(validations) {
+                this._handles = [entHandle, attrHandle, validationHandle];
+            }
+        },
 
-			this._clearValidations();
-			
-			var val = validations[0],
-				msg = val.getReasonByAttribute(this.reference.split('/')[0]);    
+        _handleValidation: function (validations) {
 
-			if(this.readOnly){
-				val.removeAttribute(this.reference.split('/')[0]);
-			} else {                                
-				if (msg) {
-					this._addValidation(msg);
-					val.removeAttribute(this.reference.split('/')[0]);
-				}
-			}
-		},
+            this._clearValidations();
 
-		_clearValidations: function() {
-			domConstruct.destroy(this._alertdiv);
-		},
+            var val = validations[0],
+                msg = val.getReasonByAttribute(this.reference.split('/')[0]);
 
-		_addValidation : function(msg) {
-			this._alertdiv = domConstruct.create("div", { 
-				class : 'alert alert-danger',
-				innerHTML: msg });
+            if (this.readOnly) {
+                val.removeAttribute(this.reference.split('/')[0]);
+            } else {
+                if (msg) {
+                    this._addValidation(msg);
+                    val.removeAttribute(this.reference.split('/')[0]);
+                }
+            }
+        },
 
-			this.domNode.appendChild(this._alertdiv);
+        _clearValidations: function () {
+            domConstruct.destroy(this._alertdiv);
+        },
 
-		},
+        _addValidation: function (msg) {
+            this._alertdiv = domConstruct.create("div", {
+                class: 'alert alert-danger',
+                innerHTML: msg
+            });
+
+            this.domNode.appendChild(this._alertdiv);
+
+        },
 
         /**
          * Interaction widget methods.
@@ -232,27 +232,27 @@ require([
          */
         _loadData: function () {
             console.debug('CheckboxSelector - Load data');
-			this._clearValidations();
+            this._clearValidations();
 
-			//default fetch
-			var refEntity = this.reference.split('/')[1],
-				filters = {},
-				xpath = '//' + refEntity;
+            //default fetch
+            var refEntity = this.reference.split('/')[1],
+                filters = {},
+                xpath = '//' + refEntity;
 
-			filters.sort = [[this.sortAttr, this.sortOrder]];
-			if (this.limit > 0) {
-				filters.amount = this.limit;
-			}
-			if (this.constraint) {
-				xpath = '//' + refEntity + this.constraint.replace('[%CurrentObject%]', this._contextObj);
-			}
-			mx.data.get({
-				xpath: xpath,
-				filter: filters,
-				callback: lang.hitch(this, function (objs) {
-					this._fetchData(objs);
-				})
-			});
+            filters.sort = [[this.sortAttr, this.sortOrder]];
+            if (this.limit > 0) {
+                filters.amount = this.limit;
+            }
+            if (this.constraint) {
+                xpath = '//' + refEntity + this.constraint.replace('[%CurrentObject%]', this._contextObj);
+            }
+            mx.data.get({
+                xpath: xpath,
+                filter: filters,
+                callback: lang.hitch(this, function (objs) {
+                    this._fetchData(objs);
+                })
+            });
 
         },
 
@@ -292,12 +292,12 @@ require([
             var tbody = domQuery('tbody', this._wgtNode)[0],
                 thead = domQuery('thead tr', this._wgtNode)[0];
             //empty the table
-			domConstruct.empty(tbody);
-			domConstruct.empty(thead);
-			
-			domConstruct.place(this._firstTh, thead, 'first'); 
+            domConstruct.empty(tbody);
+            domConstruct.empty(thead);
 
-			array.forEach(headers, function (header) {
+            domConstruct.place(this._firstTh, thead, 'first');
+
+            array.forEach(headers, function (header) {
                 var th = domConstruct.create('th', {
                     innerHTML: header
                 });
@@ -305,25 +305,24 @@ require([
             });
 
             array.forEach(rows, lang.hitch(this, function (rowData) {
-                
-				var row = domConstruct.create('tr', { 
-					id : this.domNode.id + '_' + rowData.id
-					}
-				);
-				var checkboxtd = domConstruct.create('td');
-				var input = domConstruct.create('input', {
-					type : 'checkbox',
-					value : rowData.id
-				});
 
-				domConstruct.place(input, checkboxtd);
-				domConstruct.place(checkboxtd, row);
-				
+                var row = domConstruct.create('tr', {
+                        id: this.domNode.id + '_' + rowData.id
+                    }),
+                    checkboxtd = domConstruct.create('td'),
+                    input = domConstruct.create('input', {
+                        type: 'checkbox',
+                        value: rowData.id
+                    });
+
+                domConstruct.place(input, checkboxtd);
+                domConstruct.place(checkboxtd, row);
+
                 array.forEach(rowData.data, function (value) {
-					var td = domConstruct.create('td', {
-						innerHTML: value
-					});
-					domConstruct.place(td, row, 'last');
+                    var td = domConstruct.create('td', {
+                        innerHTML: value
+                    });
+                    domConstruct.place(td, row, 'last');
                 });
                 domConstruct.place(row, tbody);
             }));
@@ -338,15 +337,15 @@ require([
             var data = [],
                 finalLength = objs.length * this.displayAttrs.length;
 
-            array.forEach(objs, lang.hitch( this, function (obj) {
-				array.forEach(this.displayAttrs, lang.hitch(this, function (attr, index) {
-					obj.fetch(attr.displayAttr, lang.hitch(this, function (value) {
+            array.forEach(objs, lang.hitch(this, function (obj) {
+                array.forEach(this.displayAttrs, lang.hitch(this, function (attr, index) {
+                    obj.fetch(attr.displayAttr, lang.hitch(this, function (value) {
                         if (typeof value === 'string') {
                             value = mxui.dom.escapeString(value);
                             value = value.replace(/<script\b[^>]*>([\s\S]*?)<\/script>/gm, ' Warning! Script tags not allowed. ');
                         }
                         if (attr.currency !== "None") {
-							value = this._parseCurrency(value, attr);
+                            value = this._parseCurrency(value, attr);
                         }
 
                         data.push({
@@ -356,7 +355,7 @@ require([
                             'header': attr.header
                         });
                         if (data.length === finalLength) {
-							this._processData(data);
+                            this._processData(data);
                         }
                     }));
                 }));
@@ -437,7 +436,7 @@ require([
             console.debug('CheckboxSelector - (De)select all checkboxes');
             var self = this;
             array.forEach(boxes, function (box) {
-                if (self._data[self.id]._selectAllBox.checked) {
+                if (self._selectAllBox.checked) {
                     box.checked = true;
                 } else {
                     box.checked = false;
@@ -455,21 +454,22 @@ require([
 
             if (Array.isArray(boxes)) {
                 array.forEach(boxes, function (box) {
+                    console.log(box);
                     id = box.value;
                     if (box.checked) {
                         console.debug('checked');
-                        self._setAsReference();
+                        self._setAsReference(id);
                     } else {
                         console.debug('CheckboxSelector - Remove references');
                         if (refguids) {
-                            self._data[self.id]._contextObj.removeReferences(referenceStr, refguids);
+                            self._contextObj.removeReferences(referenceStr, refguids);
                         }
                     }
                 });
             } else {
                 if (boxes.checked) {
                     console.debug('checked');
-					this._setAsReference(boxes.value);
+                    this._setAsReference(boxes.value);
                 } else {
                     console.debug('CheckboxSelector - Remove references');
                     this._contextObj.removeReferences(referenceStr, boxes.value);
